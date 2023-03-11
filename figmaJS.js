0098a7887d05d15e5fbe34c4f4d750f2b21ac5fd
tsarls <125039671+tsarls88@users.noreply.github.com>
2023-03-11 15:28:14 +0800

    Delete figmaJS.js

--- figmaJS.js
+++
@@ -1,8 +0,0 @@
-const togglePassword = document.querySelector('#togglePassword');
- const password = document.querySelector('#id_password');
-
- togglePassword.addEventListener('click',function(e){
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type',type);
-    this.classList.toggle('fa-eye-slash');
- });
